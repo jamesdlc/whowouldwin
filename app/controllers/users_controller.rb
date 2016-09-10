d62b2d2eb582d35f51f8b0213511d56e01962aa0
jamestothe <jamesdelacruz@gmail.com>
2016-09-09 23:59:31 -0700

    added new signup form and user controller methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UsersController < ApplicationController
+
+  # include AuthHelper
+
+  before_action :find_user, only: [:show, :edit, :update]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.image_url == ""
+      @user.image_url = "https://s-media-cache-ak0.pinimg.com/564x/dd/49/dd/dd49dd83e894321a9402465c98ebc386.jpg"
+    end
+    if @user.save
+      # login(@user)
+      redirect_to root_path
+    else
+
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.image_url == ""
+      @user.image_url = "https://s-media-cache-ak0.pinimg.com/564x/dd/49/dd/dd49dd83e894321a9402465c98ebc386.jpg"
+    end
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by_id(params[:id])
+  end
+
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :user_name, :password, :image_url)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: "users#index"
+
+get '/signup' => "users#new", as: "new_user"
+post '/users' => "users#create"
+get '/users/:id' => "users#show", as: "user"
+get '/login' => "sessions#new", as: "login"
+get '/logout' => "sessions#destroy"
+post '/sessions' => "sessions#create"
+get '/users/:id/edit' => "users#edit", as: "edit_user"
+patch '/users/:id' => "users#update"
+
 end
