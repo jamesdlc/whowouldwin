ac714e73ac6dc1b3f38d01ccc5a5bf4ca42078d4
jamestothe <jamesdelacruz@gmail.com>
2016-09-13 16:31:22 -0700

    added type coercion to get_mean logic and made user show look better

--- app/models/nba_team.rb
+++ app/models/nba_team.rb
@@ -1,7 +1,7 @@
 class NbaTeam < ApplicationRecord
 
   def mean_score_of_last_five
-    @mean_score_of_last_five ||= (scores.inject(:+) ) / scores.length
+    @mean_score_of_last_five ||= (scores.inject(:+) * 1.0) / scores.length
   end
 
 end
