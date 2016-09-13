285dd688c0483fc07e8c133705c230c57a160950
jamestothe <jamesdelacruz@gmail.com>
2016-09-13 11:34:02 -0700

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,15 @@ USERS = [
     email: "switt@yahoo.com",
     password_digest: "$2a$10$PY2ABiZo3SFpQ3xjiUdtbOMSRAQOCe7p7kM4BeYhSFj.Kpgch8KE.",
     image_url: "https://s-media-cache-ak0.pinimg.com/564x/dd/49/dd/dd49dd83e894321a9402465c98ebc386.jpg"
-  } #pw switt
+  }, #pw switt
+  {
+    user_name: "slickricktharuler",
+    first_name: "Slick",
+    last_name: "Rick",
+    email: "slick@rick.com",
+    password_digest: "$2a$10$7W4SGCYpvVnExQIt7rgI3.8gcNA1jfhNrBeRnjbW4oOT0eGKcHYg6",
+    image_url: "http://www.thefamouspeople.com/profiles/images/slick-rick-4.jpg"
+  } #pw rick
 ]
 
 DISCUSSIONS = [
@@ -30,13 +38,15 @@ NBA_TEAMS = [
     name: "New York Knicks",
     conference: "East",
     division: "Atlantic",
-    scores: [78, 81, 110, 93, 102]
+    scores: [78, 81, 110, 93, 102],
+    abbreviation: "NYK"
   },
   {
     name: "Golden State Warriors",
     conference: "West",
     division: "Pacific",
-    scores: [102, 115, 132, 108, 122]
+    scores: [102, 115, 132, 108, 122],
+    abbreviation: "GSW"
   }
 ]
 
