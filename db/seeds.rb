7b713da9dcfd8565d56bbca9f246de1a3413656d
jamestothe <jamesdelacruz@gmail.com>
2016-09-14 18:09:04 -0700

    added comments preview to comments new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ USERS = [
     last_name: "Swittaker",
     email: "switt@yahoo.com",
     password_digest: "$2a$10$PY2ABiZo3SFpQ3xjiUdtbOMSRAQOCe7p7kM4BeYhSFj.Kpgch8KE.",
-    image_url: "https://s-media-cache-ak0.pinimg.com/564x/dd/49/dd/dd49dd83e894321a9402465c98ebc386.jpg"
+    image_url: "http://i.imgur.com/49Iw8sS.jpg"
   }, #pw switt
   {
     user_name: "slickricktharuler",
